82b3684778f2fe05a4734baf045e0667cbfd3783
nana <nykim@nykim.net>
2019-11-20 14:52:23 +0900

    magnetic effect 추가

---
+++ html/magnetic.js
@@ -0,0 +1,43 @@
+
+// var nav = document.querySelector(".nav");
+// var menuArea = document.querySelector("#menu1");
+// var menulink = document.querySelector("#menu-link");
+
+var menu = document.querySelector("#testMenu");
+
+// var tester = document.querySelector("#tester");
+
+
+
+function move(e) {
+  var a = menu.getBoundingClientRect();
+  var b = e.pageX - a.left;
+  var c = e.pageY - a.top;
+
+  var x = b - (a.width / 2);
+  var y = c - (a.height / 2) - window.pageYOffset;
+  var movement = 70;
+
+  TweenMax.to(menu, 0.3, {
+    x: x / a.width * movement,
+    y: y / a.width * movement,
+    scale: 1.3,
+    ease: Power2.easeOut
+  });
+}
+
+
+
+menu.addEventListener("mousemove", function (e) {
+  move(e);
+});
+
+
+menu.addEventListener("mouseleave", function (e) {
+  TweenMax.to(menu, 0.5, {
+    x: 0,
+    y: 0,
+    scale: 1,
+    ease: Power2.easeOut
+  });
+});
---
+++ html/ui.js
@@ -0,0 +1,61 @@
+
+
+var nav = document.querySelector("#testMouse");
+var navMenu = document.querySelector(".nav-menu");
+var menu = document.querySelector("#testMenu");
+var tester = document.querySelector("#tester");
+
+
+// function mouseEnterEvt(e) {
+//   // console.log(e);
+//   // var x = e.offsetX;
+//   // menu.style.transform = `translateX(${x - 60}px)`;
+//   // e.stopPropagation();
+//   navMenu.addEventListener("mousemove", mouseMoveEvt)
+
+// }
+// function mouseMoveEvt(e) {
+//   console.log(e);
+// }
+// menu.addEventListener("mouseenter", mouseEnterEvt);
+
+
+
+function mouseMove(e) {
+  var x = e.offsetX - 90;
+  var y = 0;
+  // console.dir(e);
+  tester.innerHTML = e.offsetX;
+  menu.style.transform = "translate3d(" + x + "px," + y + "px,0)";
+  e.stopPropagation();
+}
+
+function mouseLeave(e) {
+  var x = 0;
+  var y = 0;
+  menu.style.transform = "translate3d(" + x + "px," + y + "px,0)";
+}
+
+function stopPropa(e) {
+  console.log("!");
+  e.stopPropagation();
+}
+
+// navMenu.addEventListener("mouseout", mouseLeave)
+navMenu.addEventListener("mousemove", mouseMove);
+// menu.addEventListener("mouseenter", function (e) {
+//   console.log("!");
+//   e.stopPropagation();
+// })
+// menu.addEventListener("mouseon", function (e) {
+//   console.log("!");
+//   e.stopPropagation();
+// })
+// menu.addEventListener("mousedown", function (e) {
+//   console.log("!");
+//   e.stopPropagation();
+// })
+// menu.addEventListener("mousemove", function (e) {
+//   console.log("!");
+//   e.stopPropagation();
+// })
